e81cab7376471a7bd1cf062949ba0fee15e2291f
Dennis Mattews <alttert@mail.ru>
2024-09-12 23:49:27 +0300

    logger added to main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import FastAPI
 
 from app.messenger import MessengerAPI
@@ -7,12 +9,14 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def read_root():
     return {"message": "Hello, World!"}
 
 
 @app.post("/send-message/")
 async def send_message(recipient_id: str, text: str):
+    logging.info(f"Sending message to recipient: {recipient_id}")
     messenger = MessengerAPI(page_access_token=settings.page_access_token)
     response = await messenger.send_text_message(recipient_id, text)
+    logging.info(f"Message sent with response: {response}")
     return response
